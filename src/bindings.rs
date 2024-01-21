85e69606c379f475c13a6e34f25e13dfb25b88e8
navigaid <navigaid@gmail.com>
2024-01-21 07:45:26 +0000

    0.0.1: add http wit from wasmcloud hello-world
    https://github.com/wasmCloud/wasmCloud/blob/main/examples/rust/actors/http-hello-world/wit/world.wit
    https://wasmcloud.com/docs/developer/actors/generate

--- src/bindings.rs
+++ src/bindings.rs
@@ -3,21 +3,32 @@ pub use ::http::{Method, Request, Response};
 // Load bindings from WIT file.
 // wit_bindgen_rust::import!({paths: ["../../wit/core/http.wit"]});
 wit_bindgen::generate!({
-    path: "./",
+    path: "./wit",
+    world: "hello",
     exports: {
-        "foo:bar/ihttp": Runner,
-    }
+        "wasi:http/incoming-handler": HttpServer,
+    },
 });
 
-pub struct Runner;
+use exports::wasi::http::incoming_handler::Guest;
+use wasi::http::types::*;
 
-pub use crate::bindings::exports::foo::bar::ihttp::{Guest, A};
-impl Guest for Runner {
-    fn run() -> A {
-        String::from("hello")
+pub struct HttpServer;
+
+impl Guest for HttpServer {
+    fn handle(_request: IncomingRequest, response_out: ResponseOutparam) {
+        let response = OutgoingResponse::new(Fields::new());
+        response.set_status_code(200).unwrap();
+        let response_body = response.body().unwrap();
+        response_body
+            .write()
+            .unwrap()
+            .blocking_write_and_flush(b"Hello from Rust!\n")
+            .unwrap();
+        OutgoingBody::finish(response_body, None).expect("failed to finish response body");
+        ResponseOutparam::set(response_out, Ok(response));
     }
 }
-// wit_bindgen::generate!({path: "./wasi-http/wit/proxy.wit"});
 
 /*
 use self::httpworld::{
